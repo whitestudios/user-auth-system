3d469428e33a9a1baaf63e44ae85217ddb5abecd
DarkCommit <gabrielraialsantos@gmail.com>
2025-05-23 00:01:51 -0300

    feat: add user model and repository

---
+++ internal/user/repository.go
@@ -0,0 +1,58 @@
+package user
+
+import (
+	"gorm.io/gorm"
+)
+
+type UserRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) *UserRepository {
+	return &UserRepository{
+		db: db,
+	}
+}
+
+func (rep *UserRepository) FindById(id uint) (*User, error) {
+	var u User
+
+	if err := rep.db.First(&u, id).Error; err != nil {
+		// this error can be 'user not found, handle this when you call a user repository
+		return nil, err
+	}
+
+	return &u, nil
+}
+
+func (rep *UserRepository) GetAll() ([]User, error) {
+	var users []User
+
+	if err := rep.db.Find(&users).Error; err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func (rep *UserRepository) Create(u *User) error {
+	return rep.db.Create(u).Error
+}
+
+func (rep *UserRepository) Update(u *User) error {
+	return rep.db.Save(u).Error
+}
+
+func (rep *UserRepository) DeleteById(id uint) error {
+	var user User
+
+	if err := rep.db.First(&user, id).Error; err != nil {
+		return err
+	}
+
+	return rep.db.Delete(&user).Error
+}
+
+func (rep *UserRepository) Delete(u *User) error {
+	return rep.db.Delete(&u).Error
+}
